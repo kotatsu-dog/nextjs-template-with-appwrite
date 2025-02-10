6a53dc4d552f321acfe68c1bf7e3b15b51e74433
こたついぬ <198446713+kotatsu-dog@users.noreply.github.com>
2025-02-10 20:42:17 +0900

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-
   experimental: {
     serverActions: {
-      allowedOrigins: ["psychic-invention-g4rv5prvqw74fvvxj-3000.app.github.dev", "localhost:3000"]
+      allowedOrigins: ["localhost:3000"]
     }
   }
 };
